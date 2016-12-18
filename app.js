ca65715d7bcfcc7dca1fd834df284adddb1f69fa
Omkar Lokhande <lokhandeomkar@gmail.com>
2016-12-17 18:52:52 -0800

    Update app.js

--- app.js
+++ app.js
@@ -36,6 +36,12 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // error handlers
 // development error handler
 // will print stacktrace
